96dcc491ee1abac8721fa3593841fe2f0d874260
hp <hp@DESKTOP-58D1OCI>
2020-08-30 21:10:00 +0530

    New backup now demo project Fresh 1

--- demo/src/main/java/com/saama/demo/Exception/CustomizedResponseEntityExceptionHandler.java
+++ demo/src/main/java/com/saama/demo/Exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,20 +2,14 @@ package com.saama.demo.Exception;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
